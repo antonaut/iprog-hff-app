26a8aa01e37bcdb35fa727f5b288aa3f2a843dba
Anton Erholt <anton.erholt@gmail.com>
2015-02-10 16:05:27 +0100

    Changed traversal method in left-summary.

--- js/view/left-summary.js
+++ js/view/left-summary.js
@@ -1,8 +1,8 @@
 define([], function() {
 
     var LeftSummary = function(container, lang) {
-        container.children("h5").html(lang.dishlist.HEADER);
-        container.children("label").html(lang.dishlist.PEOPLE);
+        container.find("h5").html(lang.dishlist.HEADER);
+        container.find("label").html(lang.dishlist.PEOPLE);
 
         container.find("thead td:first").html(lang.dishlist.DISH_NAME);
         container.find("thead td:last").html(lang.dishlist.COST);
