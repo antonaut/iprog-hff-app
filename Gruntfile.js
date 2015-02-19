c8d0a3c640b32f163d74b9575bb4dd86e8a6ef16
Anton Erholt <aerholt@kth.se>
2015-02-19 11:42:59 +0100

    Updated gitignore, added livereload injection

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,69 @@
 module.exports = function(grunt) {
 
+  var os = require('os');
+  var ifaces = os.networkInterfaces();
+  var host_ips = [];
+
+  Object.keys(ifaces).forEach(function (ifname) {
+    var alias = 0
+      ;
+
+    ifaces[ifname].forEach(function (iface) {
+      if ('IPv4' !== iface.family || iface.internal !== false) {
+        // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+        return;
+      }
+
+      host_ips.push(iface.address);
+
+      if (alias >= 1) {
+        // this single interface has multiple ipv4 addresses
+        console.log(ifname + ':' + alias, iface.address);
+      } else {
+        // this interface has only one ipv4 adress
+        console.log(ifname, iface.address);
+      }
+
+    });
+  });
+
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     watch: {
-      files: ["js/**/*.js", "css/**/*.css", "index.html"],
+      files: ["js/**/*.js", "css/**/*.css", "html/index.html"],
       options: {
-        livereload: true,
+        livereload: {
+          hostname: host_ips[0]
+        }
       }
     },
     connect: {
       target: {
         options: {
           port: 9000,
-          hostname: '0.0.0.0',
+          hostname: host_ips[0]
         }
       }
+    },
+    inject: {
+      single: {
+        scriptSrc: 'devScript.js',
+        files: {
+          'index.html': 'html/index.html'
+        }
+      },
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-inject');
 
   // Default task(s).
   grunt.registerTask('default', function(target) {
-    grunt.task.run(['connect', 'watch']);
+    grunt.task.run(['inject', 'connect', 'watch']);
   });
 
 };
